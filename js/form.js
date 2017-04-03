f4458b39a54164ca956a7e31a593f1bbfedfd67c
vasilkooov <gvasilkov23@gmail.com>
2017-04-03 14:21:36 +0300

    clear error field start redirect

--- js/form.js
+++ js/form.js
@@ -13,28 +13,40 @@ window.onload = function () {
 function ajaxGet() {
 	var request = new XMLHttpRequest();
 
-	var name = document.getElementById('first-name').value;
-	var secondname = document.getElementById('second-name').value;
+	var name = document.getElementById('name').value;
+	var secondname = document.getElementById('secondname').value;
 	var email = document.getElementById('email').value;
 	var gender = document.getElementById('user-gender').value;
 	var password = document.getElementById('user-pass').value;
 	var check = document.getElementById('user-check').value;
+
+
+	
+	var error_m = document.querySelectorAll('p.error');
+
+	// for (var i = 0; i < error_m.length; i++) {
+	// 	console.log(error_m[i].innerHTML);
+ //   	error_m[i].innerHTML = "";
+	// }
+	
+	function clearErrorField(element, index, array) {
+		console.log(element.innerHTML);
+   	element.innerHTML = "";
+	}
+
+	error_m.forEach(clearErrorField);
+
+
 
 	request.onreadystatechange = function() {
 		if(request.readyState == 4 && request.status == 200) {
 
 			var result = JSON.parse(request.responseText);
 
+			var error_elem = document.querySelector('input[name="' + result.field + '"] + p.error');
+			error_elem.innerHTML = result.message;
 
-		var error_elem = document.querySelector('input[name="' + result.field + '"] + p.error');
-		console.log(error_elem);
-
-		error_elem.innerHTML = result.message;
-
-		}
-
-		
-
+		} 
 	}
 
 		request.open('POST', 'https://codeit.pro/frontTestTask/user/registration', true);
